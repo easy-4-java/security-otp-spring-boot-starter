49b32085da371cbc804216aa4f96ce3634e263ca
wandl <hiwepy@163.com>
2026-05-27 11:15:03 +0800

    chore(pom): align to Spring Boot 3.2.x (JDK 17, Boot 3.2.12)

--- src/main/java/org/springframework/security/boot/otp/authentication/OTPAuthenticationFilter.java
+++ src/main/java/org/springframework/security/boot/otp/authentication/OTPAuthenticationFilter.java
@@ -18,10 +18,10 @@ package org.springframework.security.boot.otp.authentication;
 
 import java.io.IOException;
 
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 import org.oxerr.spring.security.otp.core.OTPAuthenticationToken;
 import org.springframework.security.authentication.event.InteractiveAuthenticationSuccessEvent;
